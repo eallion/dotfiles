307f0d0b09491278fe6d98e5d505479e44a16e78
Charles Chin <eallions@gmail.com>
2020-10-08 03:00:20 +0800

    update

--- Ubuntu/Shell/links.sh
+++ Ubuntu/Shell/links.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-ln -s /home/eallion/Files/Pictures /home/eallion/Pictures/Share
+/home/eallion/Downloads/Share
+/home/eallion/Documents/Share
+/home/eallion/Pictures/Share
+/home/eallion/Video/Share
+/home/eallion/Video/Share
 ln -s /home/eallion/Files/Downloads /home/eallion/Downloads/Share
-ln -s /home/eallion/Files/Music /home/eallion/Music/Share
+ln -s /home/eallion/Files/Documents /home/eallion/Documents/Share
+ln -s /home/eallion/Files/Pictures /home/eallion/Pictures/Share
 ln -s /home/eallion/Files/Video /home/eallion/Video/Share
+ln -s /home/eallion/Files/Music /home/eallion/Video/Share
