d476db025ccb63427ed199e37bf2036918bfd103
Charles Chin <eallions@gmail.com>
2021-01-20 22:11:21 +0800

    Branch windows

--- Cloud/bbr.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-wget --no-check-certificate -O /opt/bbr.sh https://github.com/teddysun/across/raw/master/bbr.sh
-chmod 755 /opt/bbr.sh
-/opt/bbr.sh
--- Debian/Shell/links.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-ln -s /home/eallion/Files/Downloads /home/eallion/Downloads/Share
-ln -s /home/eallion/Files/Pictures /home/eallion/Pictures/Share
-ln -s /home/eallion/Files/Video /home/eallion/Videos/Share
-ln -s /home/eallion/Files/Music /home/eallion/Music/Share
